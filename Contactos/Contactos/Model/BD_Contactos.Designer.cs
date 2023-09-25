ae2d328002e1c52a6fbb7dc28e38a834473ac779
SchyzoDev <carlos.correiamarques@gmail.com>
2023-09-25 17:28:22 +0100

    DB

--- Contactos/Contactos/Model/BD_Contactos.Designer.cs
+++ Contactos/Contactos/Model/BD_Contactos.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Carlos\ISTEC\1_Ano_2_Sem\P2\Programas\ProjectoFinal\Contactos\Contactos\Model\BD_Contactos.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Program Data\GitHubRepositories\Contactos\Contactos\Contactos\Contactos\Model\BD_Contactos.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
